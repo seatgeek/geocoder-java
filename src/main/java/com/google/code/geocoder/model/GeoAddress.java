bb671b90e97d4ccce6de3efa059be463ddeb838b
panchmp <panchmp>
2011-06-28 09:56:15 +0000

    remove unused class

--- src/main/java/com/google/code/geocoder/model/GeoAddress.java
+++
@@ -1,9 +0,0 @@
-package com.google.code.geocoder.model;
-
-public class GeoAddress {
-    protected GeocoderLocationType locationType;
-    protected String formattedAddress;
-    private AddressDetails addressDetails;
-    protected LatLng location;
-    protected LatLngBounds viewport;
-}
