b06c3b1871f55bc8c74c13cc60881d2578ea2605
panchmp <panchmp>
2011-07-03 21:30:46 +0000

    Issue 8:  Add support of google Google Maps API Premier user - test improvements

--- src/test/java/com/google/code/geocoder/SignatureTest.java
+++ src/test/java/com/google/code/geocoder/SignatureTest.java
@@ -16,10 +16,15 @@ public class SignatureTest extends Geocoder {
 
     @Test
     public void testSignature() {
-        final String initial = "/maps/api/geocode/json?address=New+York&sensor=false";
-        final StringBuilder url = new StringBuilder(initial);
-        geocoder.addClientIdAndSignURL(url);
+        geocoder.addClientIdAndSignURL(new StringBuilder("Some data to test re-usage of signer: bla-bla-000"));
+        geocoder.addClientIdAndSignURL(new StringBuilder("Some data to test re-usage of signer: bla-bla-001"));
+        geocoder.addClientIdAndSignURL(new StringBuilder("Some data to test re-usage of signer: bla-bla-002"));
+        geocoder.addClientIdAndSignURL(new StringBuilder("Some data to test re-usage of signer: bla-bla-003"));
+        geocoder.addClientIdAndSignURL(new StringBuilder("Some data to test re-usage of signer: bla-bla-004"));
+        geocoder.addClientIdAndSignURL(new StringBuilder("Some data to test re-usage of signer: bla-bla-005"));
 
+        StringBuilder url = new StringBuilder("/maps/api/geocode/json?address=New+York&sensor=false");
+        geocoder.addClientIdAndSignURL(url);
         Assert.assertEquals("/maps/api/geocode/json?address=New+York&sensor=false&client=clientID&signature=KrU1TzVQM7Ur0i8i7K3huiw3MsA=", url.toString());
     }
 }
